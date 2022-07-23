b9ad83c270ce39a8ee2333646addaa3bd8d249bc
Estelle <estelle@felix-creation.fr>
2022-07-23 16:16:05 +0200

    Ajout Matomo

--- themes/astra-child-omeva/functions.php
+++ themes/astra-child-omeva/functions.php
@@ -22,3 +22,27 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
+
+function astra_child_omeva_javascript() {
+    ?>
+    <!-- Matomo -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//matomo.omeva.fr/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
+    <!-- Start cookieyes banner -->
+    <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/1ec3b495baf85034d957c195/script.js"></script>
+    <!-- End cookieyes banner -->
+    <?php
+}
+add_action('wp_head', 'astra_child_omeva_javascript');
