d2807399168048ea86ed8ac072a7b1ea2305eb97
Estelle <estelle@felix-creation.fr>
2023-02-22 13:55:53 +0100

    Ajout nouvelle page equipe et image aleatoire en home.

--- themes/astra-child-omeva/functions.php
+++ themes/astra-child-omeva/functions.php
@@ -17,8 +17,9 @@ define( 'ASTRA_CHILD_THEME_VERSION', '1.0.1' );
  * Enqueue styles
  */
 function child_enqueue_styles() {
-	wp_enqueue_style( 'omeva-icon-css', get_stylesheet_directory_uri() . '/fontello.css', array('astra-theme-css'), ASTRA_CHILD_THEME_VERSION, 'all' );
+    wp_enqueue_style( 'omeva-icon-css', get_stylesheet_directory_uri() . '/fontello.css', array('astra-theme-css'), ASTRA_CHILD_THEME_VERSION, 'all' );
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), ASTRA_CHILD_THEME_VERSION, 'all' );
+	wp_enqueue_script( 'omeva-js', get_stylesheet_directory_uri() . '/script.js', array('jquery'), ASTRA_CHILD_THEME_VERSION, 'all');
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
